591f84f6a9546855042544ce2dcde60d80142798
THEGABOALE <garciaga@uamv.edu.ni>
2026-04-22 19:32:45 -0600

    feat(header): agregar sección inicial de saludo

---
+++ Practica_sesion6/dashboard_App_Habitos/app/src/main/java/ni/edu/uam/momentum/ui/components/HeaderSection.kt
@@ -0,0 +1,35 @@
+package ni.edu.uam.momentum.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HeaderSection() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column {
+            Text(text = "Hola, Ana 👋")
+            Text(text = "Vamos por un gran día")
+        }
+
+        IconButton(onClick = { }) {
+            Icon(
+                imageVector = Icons.Default.Notifications,
+                contentDescription = "Notificaciones"
+            )
+        }
+    }
+}
