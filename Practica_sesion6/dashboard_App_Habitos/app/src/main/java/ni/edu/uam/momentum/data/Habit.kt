cde3f08025cb7818fbbdb2a320acfba51cdd57b7
THEGABOALE <garciaga@uamv.edu.ni>
2026-04-23 00:46:27 -0600

    refactor(data): agregar valor por defecto al estado de habito

--- Practica_sesion6/dashboard_App_Habitos/app/src/main/java/ni/edu/uam/momentum/data/Habit.kt
+++ Practica_sesion6/dashboard_App_Habitos/app/src/main/java/ni/edu/uam/momentum/data/Habit.kt
@@ -3,6 +3,6 @@ package ni.edu.uam.momentum.data
 data class Habit(
     val name: String,
     val goal: String,
-    val completed: Boolean,
+    val completed: Boolean = false,
     val category: String
 )
