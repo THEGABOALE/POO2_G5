ec181627dbfc099eaf3c695d503c1e56ffe0fa2f
THEGABOALE <garciaga@uamv.edu.ni>
2026-04-23 01:21:47 -0600

    feat(data): ampliar modelo de metas con tipo fecha y duración

--- Practica_sesion6/dashboard_App_Habitos/app/build.gradle.kts
+++ Practica_sesion6/dashboard_App_Habitos/app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation("androidx.compose.material:material-icons-extended")
     implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation(libs.androidx.compose.foundation.layout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- Practica_sesion6/dashboard_App_Habitos/app/src/main/java/ni/edu/uam/momentum/data/Habit.kt
+++ Practica_sesion6/dashboard_App_Habitos/app/src/main/java/ni/edu/uam/momentum/data/Habit.kt
@@ -1,8 +1,17 @@
 package ni.edu.uam.momentum.data
 
+enum class HabitType {
+    MANUAL,
+    TIMER,
+    DEADLINE
+}
+
 data class Habit(
     val name: String,
     val goal: String,
     val completed: Boolean = false,
-    val category: String
+    val category: String,
+    val type: HabitType = HabitType.MANUAL,
+    val dueDate: String? = null,
+    val duration: String? = null
 )
