d56c16f943b2efca41c555e9b68716f0893215f8
THEGABOALE <garciaga@uamv.edu.ni>
2026-04-23 01:25:26 -0600

    refactor(app): convertir lista de metas en estado mutable compartido

--- Practica_sesion6/dashboard_App_Habitos/app/src/main/java/ni/edu/uam/momentum/ui/screen/AppScreen.kt
+++ Practica_sesion6/dashboard_App_Habitos/app/src/main/java/ni/edu/uam/momentum/ui/screen/AppScreen.kt
@@ -3,6 +3,7 @@ package ni.edu.uam.momentum.ui.screen
 import android.net.Uri
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -13,7 +14,7 @@ fun AppScreen() {
     var hasStarted by remember { mutableStateOf(false) }
     var userName by remember { mutableStateOf("") }
     var profileImageUri by remember { mutableStateOf<Uri?>(null) }
-    var habits by remember { mutableStateOf(listOf<Habit>()) }
+    val habits = remember { mutableStateListOf<Habit>() }
 
     if (!hasStarted) {
         WelcomeScreen { name, imageUri ->
@@ -25,7 +26,10 @@ fun AppScreen() {
         DashboardScreen(
             userName = userName,
             profileImageUri = profileImageUri,
-            habits = habits
+            habits = habits,
+            onAddHabit = { newHabit ->
+                habits.add(newHabit)
+            }
         )
     }
 }
